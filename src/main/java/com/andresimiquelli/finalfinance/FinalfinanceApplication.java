bae56843fbe586fa8f8da17943595689612c1fa5
André Simiquelli <andresimiquelli@hotmail.com>
2022-01-16 17:35:00 -0300

    configuração de segurança CORS - primeiro commit

---
+++ src/main/java/com/andresimiquelli/finalfinance/FinalfinanceApplication.java
@@ -0,0 +1,13 @@
+package com.andresimiquelli.finalfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalfinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalfinanceApplication.class, args);
+	}
+
+}
